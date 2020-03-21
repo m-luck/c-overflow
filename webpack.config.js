1bec1d6e4c09f62d708434482e4ab447ebf36082
Michael Luckyman <lukiman@quorum.us>
2020-03-21 15:52:28 -0700

    Instantiate bare project

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'main.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
