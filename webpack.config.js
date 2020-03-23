e0772d3f26cfaf5529d22633ff98ebdff2b7d51c
Michael Luckyman <lukiman@quorum.us>
2020-03-22 22:10:30 -0700

    Reorg for Amplify

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 module.exports = {
-    entry: './src/index.js',
+    entry: './frontend-overflow/src/index.js',
     output: {
-        path: path.resolve('dist'),
+        path: path.resolve('frontend-overflow/dist'),
         filename: 'bundle.js'
     },
     module: {
